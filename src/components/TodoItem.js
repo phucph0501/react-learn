eebe868448cb0c2b6aeb31398e66fe826c9d6457
phucph0501 <42713813+phucph0501@users.noreply.github.com>
2022-09-13 10:52:59 +0700

    2 files modified, 1 added

---
+++ src/components/TodoItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+  render() {
+    return <li className="todo-item">{this.props.todo.title}</li>;
+  }
+}
+export default TodoItem;
