e9c9c71f9db45933a5eab3d750372b99bb15cce6
phucph0501 <42713813+phucph0501@users.noreply.github.com>
2022-09-14 10:24:11 +0700

    2 files modified

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class AddTodo extends React.Component {
-  onInputChange = (e) => {
-    this.setState({
-      title: e.target.value,
-    });
+function AddTodo(props) {
+  const [title, setTitle] = useState('');
+  const onInputChange = (e) => {
+    setTitle(e.target.value);
   };
-  addTodo = (e) => {
+  const addTodo = (e) => {
     e.preventDefault();
-    this.props.addTodo(this.state.title);
-    this.setState({
-      title: '',
-    });
+    props.addTodo(title);
+    setTitle('');
   };
-  state = {
-    title: '',
-  };
-  render() {
-    return (
-      <form className="form-container" onSubmit={this.addTodo}>
-        <input
-          type="text"
-          placeholder="Add Todo..."
-          className="input-text"
-          value={this.state.title}
-          onChange={this.onInputChange}
-        ></input>
-        <input type="submit" value="Submit" className="input-submit"></input>
-      </form>
-    );
-  }
+  return (
+    <form className="form-container" onSubmit={addTodo}>
+      <input
+        type="text"
+        placeholder="Add Todo..."
+        className="input-text"
+        value={title}
+        onChange={onInputChange}
+      ></input>
+      <input type="submit" value="Submit" className="input-submit"></input>
+    </form>
+  );
 }
+
 export default AddTodo;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class TodoItem extends React.Component {
-  render() {
-    const { id, title, completed } = this.props.todo;
-    return (
-      <li className="todo-item">
-        <input
-          type="checkbox"
-          checked={completed}
-          onChange={() => this.props.handleChange(id)}
-        />
-        <span className={completed ? 'completed' : null}>{title}</span>
-        <button className="btn-style" onClick={() => this.props.deleteTodo(id)}>
-          X
-        </button>
-      </li>
-    );
-  }
+function TodoItem(props) {
+  const { id, title, completed } = props.todo;
+  return (
+    <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => props.handleChange(id)}
+      />
+      <span className={completed ? 'completed' : null}>{title}</span>
+      <button className="btn-style" onClick={() => props.deleteTodo(id)}>
+        X
+      </button>
+    </li>
+  );
 }
+
 export default TodoItem;
